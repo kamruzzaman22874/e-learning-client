0f1d211a37f2cb12c792a43b9dbf511bc7c21bdd
Md. Jamal Hossen <joybd.eee@gmail.com>
2023-09-30 22:07:12 +0600

    added some code

--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -1,20 +1,15 @@
 const Course = ({ course }) => {
-    const { courseName, courseImage }= course;
+    const { courseName, courseContent, courseImage }= course;
     return (
         <section className="pt-10 overflow-hidden my-20 bg-gray-50 md:pt-0 sm:pt-16 2xl:pt-16">
             <div className="px-4 mx-auto sm:px-6 lg:px-6 max-w-7xl">
                 <div className="grid items-center grid-cols-1 md:grid-cols-2">
                     <div>
-                        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">{courseName}</h2>
-                        <p className="max-w-lg mt-3 text-xl leading-relaxed text-gray-600 md:mt-8">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-
-                        <p className="mt-4 text-xl text-gray-600 md:mt-8">
-                            <span className="relative inline-block">
-                                <span className="absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300"></span>
-                                <span className="relative"> Have a question? </span>
-                            </span>
-                            <br className="block sm:hidden" />Ask me on <a href="#" title="" className="transition-all duration-200 text-sky-500 hover:text-sky-600 hover:underline">Twitter</a>
-                        </p>
+                        <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-3xl">{courseContent}</h2>
+                        <h2 className="py-5 font-semibold">{courseName}</h2>
+                        <div className="my-10">
+                            <button className="custom-gradient px-6 py-3 rounded text-white">Course Details</button>
+                        </div>
                     </div>
 
                     <div className="relative">
